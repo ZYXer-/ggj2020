605ff84d923165d07c5de9094c8db45ca9be4507
Jan Meier <janmeier0.o@gmail.com>
2020-02-29 17:04:41 +0100

    master: commmit eslintrc

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+    "extends": "airbnb",
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': 'eslint:recommended',
+    'globals': {
+        'Atomics': 'readonly',
+        'SharedArrayBuffer': 'readonly'
+    },
+    'parserOptions': {
+        'ecmaVersion': 2018,
+        'sourceType': 'module'
+    },
+    'rules': {
+        'indent': [
+            'error',
+            4
+        ],
+        'linebreak-style': [
+            'error',
+            'unix'
+        ],
+        'quotes': [
+            'error',
+            'single'
+        ],
+        'semi': [
+            'error',
+            'always'
+        ],
+        'prefer-template': [
+            'error',
+        ],
+        'template-curly-spacing':  [
+            'error',
+            'never',
+        ]
+    }
+};
